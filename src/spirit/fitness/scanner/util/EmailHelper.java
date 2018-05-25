b3fe86460f6da1ac2876d8d5b15a1168f959f8c7
SPIRITLOCAL\geminih <gemini612gemini@gmail.com>
2018-05-24 19:45:07 -0500

    Highlight red color to SN that our of container range.

--- src/spirit/fitness/scanner/util/EmailHelper.java
+++ src/spirit/fitness/scanner/util/EmailHelper.java
@@ -59,21 +59,27 @@ public class EmailHelper {
 			int startIndex = 0;
 			for (Containerbean container : containInfo) {
 				String itemsArray = "";
-				String containInfoTxt =  CONTAINER_NO + container.ContainerNo + "<br>" + SCANNED_DATE + scanDate + "<br>"
+				String containInfoTxt =  CONTAINER_NO + container.ContainerNo + "<br>" + RECEIVED_DATE + containInfo.get(0).date + "<br>" + SCANNED_DATE + scanDate + "<br>"
 						+ MODEL_NO + container.SNEnd.substring(0, 6) + "<br>" + MODEL_DESC + Constrant.models.get(container.SNEnd.substring(0, 6)).Desc + "<br>"
 						+ SERIAL_START + container.SNBegin + "<br>" + SERIAL_END + container.SNEnd + "<br>" + QTY
 						+ String.valueOf(Integer.valueOf(container.SNEnd.substring(10, 16))
 								- Integer.valueOf(container.SNBegin.substring(10, 16)) + 1)
 						+ "<br>";
 
-				int qty = Integer.valueOf(container.SNEnd.substring(10, 16))- Integer.valueOf(container.SNBegin.substring(10, 16)) + 1;
+				int startIdx = Integer.valueOf(container.SNBegin.substring(10, 16)) ;
+				int endIdx = Integer.valueOf(container.SNEnd.substring(10, 16)) ;
+				
+				int qty = endIdx - startIdx+ 1;
 				String[] scanSN = new String[qty];
 				System.arraycopy( scanitems, startIndex, scanSN, 0, qty );
 				startIndex= startIndex + qty;
 				
 				for(String s : scanSN) 
 				{
-					itemsArray += s+"<br>";
+					if(Integer.valueOf(s.substring(10,16)) - startIdx < 0 || endIdx - Integer.valueOf(s.substring(10,16)) <0)
+						itemsArray += "<font color=\"red\">"+s + "</font>"+"<br>";
+					else
+						itemsArray += s+"<br>";
 				}
 				containInfoTxt += "------------------------------------------------------------<br>" + "Scanned SN : <br>"+"------------------------------------------------------------<br>" + itemsArray;
 
