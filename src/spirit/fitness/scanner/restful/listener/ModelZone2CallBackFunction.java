65167b8fbf6bd7431857463c61f00350ea3e29fa
SPIRITLOCAL\geminih <gemini612gemini@gmail.com>
2018-07-25 15:35:03 -0500

    Added Pickup function to list all stock according to zone code.

--- src/spirit/fitness/scanner/restful/listener/ModelZone2CallBackFunction.java
+++ src/spirit/fitness/scanner/restful/listener/ModelZone2CallBackFunction.java
@@ -7,10 +7,12 @@ import spirit.fitness.scanner.model.Historybean;
 import spirit.fitness.scanner.model.Itembean;
 import spirit.fitness.scanner.model.ModelDailyReportbean;
 import spirit.fitness.scanner.model.ModelZone2bean;
+import spirit.fitness.scanner.model.PickUpZoneMap;
 
 
 public interface ModelZone2CallBackFunction {
 	public void resultCode(int code);
 	public void getReportItems(List<ModelZone2bean> items);
 	public void getModelDailyReportItems(List<ModelDailyReportbean> items);
+	public void pickUpZone(List<PickUpZoneMap> items);
 }
