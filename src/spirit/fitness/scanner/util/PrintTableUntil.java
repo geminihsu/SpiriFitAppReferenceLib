221b8d8907b3b51d0a89357e2918a4a653872bf7
SPIRITLOCAL\geminih <gemini612gemini@gmail.com>
2018-09-21 09:23:01 -0500

    no message

--- src/spirit/fitness/scanner/util/PrintTableUntil.java
+++ src/spirit/fitness/scanner/util/PrintTableUntil.java
@@ -8,7 +8,7 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
 
-public class PrintJtableUtil
+public class PrintTableUntil
 {
 	/**
      * Print the table result.
